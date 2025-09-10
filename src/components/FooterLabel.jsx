ff0da8c7c692147a4f120fe9751d0d67c10bf85e
HardiikGit <hardiik003@gmail.com>
2025-09-10 16:00:25 +0530

    n

--- src/components/FooterLabel.jsx
+++ src/components/FooterLabel.jsx
@@ -7,8 +7,8 @@ export default function Footer() {
                <div className="container">
                     <div className="Footer d-flex align-items-center justify-space-between">
                          <div className="social-links d-flex align-items-center">
-                              <div className="social-name"><a href="https://github.com/Hardiik-git">Github</a></div>
-                              <div className="social-name"><a href="https://www.linkedin.com/in/har-dik-033229363/">Linkedin</a></div>
+                              <div className="social-name"><a href="https://github.com/HardiikGit">Github</a></div>
+                              <div className="social-name"><a href="https://www.linkedin.com/in/hardik-033229363/">Linkedin</a></div>
                               <div className="social-name"><Link to='/projects'>Projects</Link></div>
                          </div>
                          <div className='copyright'>
